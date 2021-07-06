e1068df72fb92c12c33ffcb7c92d40dd7a4562a4
MinhHa Pham <minhha@MinhHas-MacBook-Air.local>
2021-07-06 11:17:25 -0700

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
-var buttonEl = document.querySelector("#save-task");
+// var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 /*
@@ -10,10 +11,14 @@ buttonEl.addEventListener("click", function() {
 });
 */
 
-var createTaskHandler = function () {
+var createTaskHandler = function (event) {
+
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
-    listItemEl.textContent="This is a new task";
     listItemEl.className="task-item";
+    listItemEl.textContent="This is a new task";
     tasksToDoEl.appendChild(listItemEl);
 }
-buttonEl.addEventListener("click", createTaskHandler);
+// buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
